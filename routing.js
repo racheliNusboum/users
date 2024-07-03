2961785273166b12573f9901a8d6a25ee4d04b4f
st0534 <estypwolak>
2024-07-03 18:07:22 +0300

    add the files: controlers and routing

---
+++ routing.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const userController = require('./Controler/UserControler');
+
+router.delete('/deleteUSer/:userId',userController.deleteUserByID);
+router.post('/addUser',express.json(), userController.createNewUser);
+router.put('/addUser', userController.updateUserById);
+
+module.exports = router;
